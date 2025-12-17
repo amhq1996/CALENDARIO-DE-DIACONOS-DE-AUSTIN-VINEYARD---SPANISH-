2bbbb133feda94174ac755fdc3798b6e4ec86823
ANA MARGARITA HENRIQUEZ QUESADA <76023058+amhq1996@users.noreply.github.com>
2025-12-16 20:22:29 -0600

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,8 +1,8 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 // 🔹 SUPABASE CONNECTION
-const SUPABASE_URL = "PASTE_YOUR_PROJECT_URL_HERE";
-const SUPABASE_KEY = "PASTE_YOUR_ANON_KEY_HERE";
+const SUPABASE_URL = 'https://hpsrrjrxtlmwfjjepbkr.supabase.co';
+const SUPABASE_KEY = process.env.SUPABASE_KEY;
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 let events = [];
@@ -95,3 +95,4 @@ document.getElementById("nextWeek").onclick = () => {
 // INIT
 await loadEvents();
 renderWeek();
+
